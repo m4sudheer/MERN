f673ee7ed654c6ff5407c023aaab1843e5b21bbc
m4sudheer <m4sudheer@gmail.com>
2019-12-31 15:26:51 +0530

    mern code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+//reading .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`app is listening at port ${PORT}`);
+});
